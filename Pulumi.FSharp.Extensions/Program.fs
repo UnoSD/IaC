bd0cbfc8e755c27964ac113f0359fe1ab87852d2
UnoSD <unosd.dev@gmail.com>
2021-04-17 23:03:26 +0100

    Add PFE example

---
+++ Pulumi.FSharp.Extensions/Program.fs
@@ -0,0 +1,31 @@
+﻿module Program
+
+open Pulumi.FSharp.AzureNative.Storage.Inputs
+open Pulumi.FSharp.AzureNative.Resources
+open Pulumi.FSharp.AzureNative.Storage
+open Pulumi.AzureNative.Storage
+open Pulumi.FSharp
+
+let infra () =
+    let rg =
+        resourceGroup {
+            name               "rg-pulumi"
+            location           "West Europe"
+        }
+
+    storageAccount {
+        name                   "sa"
+        resourceGroup          rg.Name
+        location               rg.Location
+        enableHttpsTrafficOnly true
+        kind                   Kind.StorageV2
+        
+        sku {
+            name SkuName.Standard_LRS
+        }            
+    }
+
+    dict []
+
+[<EntryPoint>]
+let main _ = Deployment.run infra
